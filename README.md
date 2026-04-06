6b4ff318526dd12e5374085fa8771fbe9e90cce7
Vinicius Hespanha <mail@vhespanha.dev>
2026-04-06 18:18:16 -0300

    Scaffold project

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # zod-deno-semver
 
 Utility library adding support for semantic validation of SemVer strings.
+
+## Usage
+
+Declare your SemVer schema:
+
+```typescript
+const schema = z.semver().greaterThan('0.1.2');
+```
+
+Validate it:
+
+```typescript
+schema.parse('0.1.3'); // Valid
+schema.parse('0.0.1'); // Not valid
+```
--- main.ts
+++ main.ts
@@ -1,8 +1,4 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+import * as core from '@zod/zod/v4/core';
+import type { SemVer } from '@std/semver';
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+// ...
